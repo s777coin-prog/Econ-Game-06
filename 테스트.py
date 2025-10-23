68db5df71dce9e7d1944efedeaa1769d74037c73
s25294713-ui <s25294713@sjajeju.kr>
2025-10-23 15:46:18 +0900

    Add files via upload
    파일 올리는 법을 위한 테스트 파일.

---
+++ 테스트.py
@@ -0,0 +1,12 @@
+print (100)
+animal = 1
+print (1000)
+print (10>1000)
+
+#test1
+number1 = 20
+number2= 11
+
+print (+number1+ + +number2)
+print ("20은") + (str(+number1)), ("이다")
+print (not False)
